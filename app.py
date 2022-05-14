4a0f9f36dfeefad11a133f4c9a3c0ef104f0972d
maDenis121 <u1967080@campus.udg.edu>
2022-05-14 22:33:28 +0200

    Probando método nuevo

--- app.py
+++ app.py
@@ -6,16 +6,21 @@ def index():
     return render_template("main.html")
 
 #Importar parquetes necesarios
-#import os
-import cv2
+import os
+import cv2 
+import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
+import sys
+from PIL import Image
 #import mediapipe as mp
+import shutil
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
-def eliminarFondo():
+def eliminarFondo1():
     #cargar imagen
     img = cv2.imread('OIP.jpg', cv2.IMREAD_COLOR)
     
@@ -42,10 +47,72 @@ def eliminarFondo():
     #ponemos la máscara en el canal aplha
     result = img.copy()
     result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
-    result[:,:,3] = mask
+    result[:, :, 3] = mask
 
-    cv2.imshow('imagen',result)
+    cv2.imshow('imagen',mask)
     cv2.waitKey(0)
     cv2.destroyAllWindows
 
-eliminarFondo()
+
+def prueba2():
+    img = cv.imread('OIP.jpg', cv.IMREAD_UNCHANGED)
+    original = img.copy()
+
+    l = int(max(5, 6))
+    u = int(min(6, 6))
+
+    ed = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    edges = cv.GaussianBlur(img, (21, 51), 3)
+    edges = cv.cvtColor(edges, cv.COLOR_BGR2GRAY)
+    edges = cv.Canny(edges, l, u)
+
+    _, thresh = cv.threshold(edges, 0, 255, cv.THRESH_BINARY  + cv.THRESH_OTSU)
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+    mask = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel, iterations=4)
+
+    data = mask.tolist()
+    sys.setrecursionlimit(10**8)
+    for i in  range(len(data)):
+        for j in  range(len(data[i])):
+            if data[i][j] !=  255:
+                data[i][j] =  -1
+            else:
+                break
+        for j in  range(len(data[i])-1, -1, -1):
+            if data[i][j] !=  255:
+                data[i][j] =  -1
+            else:
+                break
+    image = np.array(data)
+    image[image !=  -1] =  255
+    image[image ==  -1] =  0
+
+    mask = np.array(image, np.uint8)
+
+    result = cv.bitwise_and(original, original, mask=mask)
+    result[mask ==  0] =  255
+    cv.imwrite('bg.png', result)
+
+    img = Image.open('bg.png')
+    img.convert("RGBA")
+    datas = img.getdata()
+
+    newData = []
+    for item in datas:
+        if item[0] ==  255  and item[1] ==  255  and item[2] ==  255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+
+    img.putdata(newData)
+    img.save("img.png", "PNG")
+
+
+def prueba3():
+    pass
+
+
+#prueba2()
+#eliminarFondo1()
+
+prueba3()
